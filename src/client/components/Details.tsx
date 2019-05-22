5b470961309648691c14e7b43d562324949de77c
Bradley <Elizabeth.Bradley@flhealth.gov>
2019-05-22 13:37:49 -0400

    works! TODO: improve data in routes.ts

--- src/client/components/Details.tsx
+++ src/client/components/Details.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import { Album } from './Albums';
+import { Album } from './Albums'; // import interface
 import { RouteComponentProps }  from 'react-router-dom';
 
+
+// interface MatchParams {
+//     id: string;
+// }
 // pass in match object generic <id: string;
-export interface DetailsProps extends RouteComponentProps<{ id: string;}> { }
+export interface DetailsProps extends RouteComponentProps<{
+    id: string;
+}> { }
 
 // passing a route parameter called id across this Route (see App.tsx)
 
@@ -13,9 +19,11 @@ export interface DetailsProps extends RouteComponentProps<{ id: string;}> { }
 // match and off of that I want params and off of that I want id.
 // So now I can just call id rather than props.match.params.id
 
-// const Details: React.SFC<DetailsProps> = (props) => {
-const Details: React.SFC<DetailsProps> = ({match: { params: { id } } }) => {
+ // const Details: React.SFC<DetailsProps> = (props) => {
+ const Details: React.SFC<DetailsProps> = ({match: { params: { id } } }) => {
+  // console.log('details **** '+ props.match.params.id);
 
+  // blank object so assign default values
   const [ album, setAlbum ] = useState<Album>({
     id: null,
     title: null,
@@ -26,22 +34,30 @@ const Details: React.SFC<DetailsProps> = ({match: { params: { id } } }) => {
     _created: null
   });
 
+  const getAlbum = async () => {
+    // let r = await fetch(`/api/albums/${props.match.params.id)}`);
+    let r = await fetch(`/api/albums/${id}`);
+    let album = await r.json();
+    setAlbum(album);
+  }
+
+  useEffect( () => { getAlbum(); }, [id]);
 
   return (
     <section className="row my-5">
       <article className="col-md-12">
         <div className="card m-1 p-1 shadow">
-        {/* <img src={album.image} alt="album image" className="card-img-top w-50 h-50 d-black mx-auto" />*/}
+          <img src={album.image} alt="album image" className="card-img-top w-50 h-50 d-black mx-auto" />
           <div className="card-body text-center">
-            {/* <h4 className="card-title">{ablum.title}</h4> */}
+            <h4 className="card-title">{album.title}</h4>
             <div className="d-flex justify-context-center align-items-center mb-2">
-                {/* <p className="card-title">{album.artist} </p>*/}
-                {/* <img src={album.thumnail_image} alt="artist image" style={{ height: '8%', width: '8%'}} className="mx-3 border"/>*/}
+                <p className="card-title">{album.artist} </p>
+                <img src={album.thumbnail_image} alt="artist image" style={{ height: '8%', width: '8%'}} className="mx-3 border"/>
             </div>
-              {/* <a href={album.url} target="_blank" className="btn btn-secondary shado btn-block mx-auto">Buy on Amazon</a>*/}
+              <a href={album.url} target="_blank" className="btn btn-secondary shadow btn-block mx-auto">Buy on Amazon</a>
               {/* */}
             <button
-              // onClick={}
+              onClick={ () => history.back() }
               className="btn btn-warning btn-block shadow mx-auto">Go Back</button>
           </div>
         </div>
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -25,8 +25,36 @@ router.get('/api/albums', (req, res, next) => {
     }
   ]
     //let albums = data.map(ablum => ({ value: album.id, text: album.title}));
-    console.log(data)
+    // console.log(data)
     res.json(data);
 });
 
+router.get('/api/albums/:id', (req, res, next) => {
+    let reqId = 0;
+    reqId = Number(req.params.id);
+    //console.log(' in get /api/albums/:id ' + req.params.id + ' = ' + reqId);
+    let data = [{
+      id: 1,
+      title: 'Aladdin',
+      artist: 'Disney',
+      url: 'https://www.amazon.com/Aladdin-Original-Motion-Picture-Soundtrack/dp/B07RHQVPDQ/ref=zg_bs_digital-music-album_15?_encoding=UTF8&psc=1&refRID=DDG6A6FD70VKZ20EHNT4',
+      image: 'http://placekitten.com/200/300'
+    }, {
+      id: 2,
+      title: 'Macarena'
+    },
+    {
+      id: 3,
+      title: 'All Star'
+    },
+    {
+      id: 4,
+      title: 'I Want It That Way'
+    }
+  ]
+    //let albums = data.map(ablum => ({ value: album.id, text: album.title}));
+    let album = data.find(item => item.id === reqId);
+    console.log(album)
+    res.json(album);
+});
 export default router;
